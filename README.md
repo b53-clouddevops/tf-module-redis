c01f1ac7dec6818f8d1eac347b852dcd73689f79
CodingManoj <mvp2612@hotmail.com>
2023-04-24 23:27:39 -0400

    Git Auto Push

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tf-module-redis
+
+This repository contains all the code needed to created Redis using AWS Managed Service Elastic-Cache
---
+++ security-group.tf
@@ -0,0 +1,33 @@
+# Creates Security Group 
+resource "aws_security_group" "allow_redis" {
+  name               = "robot-${var.ENV}-redis-sg"
+  description        = "Allows redis Internal inbound traffic"
+  vpc_id             = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description      = "Allows docdb from local network"
+    from_port        = 6379
+    to_port          = 6379
+    protocol         = "tcp"
+    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
+  }
+
+  ingress {
+    description      = "Allows docdb from default network"
+    from_port        = 6379
+    to_port          = 6379
+    protocol         = "tcp"
+    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "robot-${var.ENV}-redis-sg"
+  }
+}
