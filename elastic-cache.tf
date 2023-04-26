f29a42b58c56d6e0635f71bd5c7cae66ae9c1014
CodingManoj <mvp2612@hotmail.com>
2023-04-25 22:10:36 -0400

    Git Auto Push

--- elastic-cache.tf
+++ elastic-cache.tf
@@ -4,7 +4,7 @@ resource "aws_elasticache_cluster" "redis" {
   node_type            = "cache.t3.small"
   num_cache_nodes      = 1
   parameter_group_name = aws_elasticache_parameter_group.default.name
-  engine_version       = "6.2"
+  engine_version       = "6.x"
   port                 = 6379
   security_group_ids   = [aws_security_group.allow_redis.id]
 }
@@ -12,7 +12,7 @@ resource "aws_elasticache_cluster" "redis" {
 # Creates Parameter group
 resource "aws_elasticache_parameter_group" "default" {
   name                 = "robot-${var.ENV}-redis-pg"
-  family               = "redis6.2"
+  family               = "redis6.x"
 }
 
 # Creates Subnet Group.
