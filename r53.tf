ca27f931af7f49c30a417918334a63c49ca814a1
CodingManoj <mvp2612@hotmail.com>
2023-04-26 22:48:02 -0400

    Git Auto Push

--- r53.tf
+++ r53.tf
@@ -1,7 +1,7 @@
 # # This created the CNAME Record for Redis Elastic Cache
 resource "aws_route53_record" "redis" {
   zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
-  name    = "redis-${var.ENV}.data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME"
+  name    = "redis-${var.ENV}"
   type    = "CNAME"
   ttl     = 10
   records = [aws_elasticache_cluster.redis.cache_nodes[0].address]
