e2a791c1e8994016ac708b6cdd0d83a94b305394
CodingManoj <mvp2612@hotmail.com>
2023-04-26 23:25:43 -0400

    Git Auto Push

--- elastic-cache.tf
+++ elastic-cache.tf
@@ -2,7 +2,7 @@ resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "robot-${var.ENV}-redis"
   engine               = "redis"
   node_type            = var.ELASTIC_CACHE_NODE_TYPE
-  num_cache_nodes      = vae.ELASTIC_CACHE_NODE_COUNT
+  num_cache_nodes      = var.ELASTIC_CACHE_NODE_COUNT
   parameter_group_name = aws_elasticache_parameter_group.default.name
   engine_version       = var.ELASTIC_CACHE_ENGINE_VERSION
   port                 = var.ELASTIC_CACHE_PORT
