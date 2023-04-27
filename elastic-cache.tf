bc2baf02db6e42bca300b25e978c5b1bb7da35a8
CodingManoj <mvp2612@hotmail.com>
2023-04-26 23:24:21 -0400

    Git Auto Push

--- elastic-cache.tf
+++ elastic-cache.tf
@@ -1,11 +1,11 @@
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "robot-${var.ENV}-redis"
   engine               = "redis"
-  node_type            = "cache.t3.small"
-  num_cache_nodes      = 1
+  node_type            = var.ELASTIC_CACHE_NODE_TYPE
+  num_cache_nodes      = vae.ELASTIC_CACHE_NODE_COUNT
   parameter_group_name = aws_elasticache_parameter_group.default.name
-  engine_version       = "6.x"
-  port                 = 6379
+  engine_version       = var.ELASTIC_CACHE_ENGINE_VERSION
+  port                 = var.ELASTIC_CACHE_PORT
   security_group_ids   = [aws_security_group.allow_redis.id]
   subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
 }
--- vars.tf
+++ vars.tf
@@ -1,1 +1,6 @@
 variable "ENV" {}
+# Elasticcache Variables 
+variable "ELASTIC_CACHE_PORT" {}
+variable "ELASTIC_CACHE_NODE_TYPE" {}
+variable "ELASTIC_CACHE_NODE_COUNT" {}
+variable "ELASTIC_CACHE_ENGINE_VERSION" {}
